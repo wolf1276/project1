3124574fd57a76c161fad605425b7d29349b48b8
wolf1276 <120487235+wolf1276@users.noreply.github.com>
2025-09-17 05:01:39 +0530

    3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+import dotenv from "dotenv";
+dotenv.config({
+  path: "./.env",
+});
+let name = process.env.MY_USERNAME;
+console.log("value: ", name);
 console.log("backend projects");
+console.log("check1");
